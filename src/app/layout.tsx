f82e92655b0572868fbb35dc6411e8ea4b161164
Manoj Pudasaini <mpudasaini17@gmail.com>
2024-06-03 23:33:02 +0545

    feat: implement header and footer in root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,13 @@ import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import Header from "@/components/header";
+import Footer from "@/components/footer";
 
-const montserrat = Montserrat({ subsets: ["latin"] });
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700", "800", "900"],
+});
 
 export const metadata: Metadata = {
   title: "Chautary",
@@ -18,7 +23,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={cn(montserrat.className, "antialiased")}>
+        <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
