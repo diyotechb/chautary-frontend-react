d5295c785dede8c100271ec985d319016c3e739c
Manoj Pudasaini <mpudasaini17@gmail.com>
2024-06-04 20:17:44 +0545

    feat: responsive home page background image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,20 +2,16 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="relative flex h-[90vh] w-full items-center justify-start">
+    <main className="flex h-[90vh] w-full items-center justify-start">
       <Image
         src="/assets/img/main-banner-bg.jpg"
         alt=""
-        fill
-        className="absolute inset-0 -z-10 h-full w-full object-cover object-center"
-        sizes="100vw"
+        layout="responsive"
+        height={1080}
+        width={1920}
+        className="-mt-2 min-h-full w-full object-cover object-left"
         draggable={false}
       />
-      <search className="px-48">
-        <h3 className="text-4xl font-bold leading-10 tracking-wide">
-          Explore your favorites effortlessly!
-        </h3>
-      </search>
     </main>
   );
 }
