886c40f0d33190a0e1db9ccec7c10718677a4a5c
Manoj Pudasaini <mpudasaini17@gmail.com>
2024-06-11 22:59:10 +0545

    feat: create axios interceptor

---
+++ src/utils/api.ts
@@ -0,0 +1,27 @@
+import axios, { type AxiosError } from "axios";
+
+const API = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+API.interceptors.request.use(
+  (config) => {
+    return config;
+  },
+  (error: AxiosError) => {
+    return Promise.reject(error.response);
+  },
+);
+
+API.interceptors.response.use(
+  (response) => {
+    return Promise.resolve(response.data?.data);
+  },
+  (error: AxiosError) => {
+    return Promise.reject(error.response);
+  },
+);
+export { API };
