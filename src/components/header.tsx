155c3b32f8a1a8630d04c2d9803e3c5f880eb1c8
manojpudasaini <mpudasaini17@gmail.com>
2024-06-25 08:49:40 +0545

    fix: header image height

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -61,7 +61,7 @@ const Header = () => {
           height={30}
           width={130}
           alt="chautary"
-          className="shrink-0"
+          className="h-10 shrink-0 object-contain"
           quality={100}
         />
       </Link>
